6a6246971ce8e20e274aa9e98d06c60492079d44
gomesfilipe <filipejvmf@gmail.com>
2023-02-04 10:24:37 -0300

    removing comments

--- email/app/mailers/post_mailer.rb
+++ email/app/mailers/post_mailer.rb
@@ -1,15 +1,9 @@
-# require 'date'
-# require '../../../news-api/newsApi.rb'
-# require_relative '../../../news-api/newsApi.rb'
-
 class PostMailer < ApplicationMailer
-  # include NEWS_API
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.post_mailer.send_mail.subject
-  #
-  # sendmail.algumacoisa(user: sdfjdfjskdj, )
+
   
   def send_mail
     category = 'technology'
@@ -25,27 +19,10 @@ class PostMailer < ApplicationMailer
     @day = DateTime.now.day.to_s.rjust(2, '0')
     @month = DateTime.now.month.to_s.rjust(2, '0')
     @year = DateTime.now.year
-    
-    # @token = NEWS_API::API_KEY
-
-    # puts "######### #{NewsapiHelper} ##########"
-    # @greeting = "Hi"
 
     mail(
       to: @user.email,
       subject: "Terubytes Newsletter #{@day}/#{@month}/#{@year}"
     )
-
-    # mail(
-    #   to: "filipejvmf@gmail.com",
-    #   cc:"elainediaspires16@gmail.com",
-    #   subject: "Newsletter"
-    # )
-    # mail to: User.first.email
-  end
-
-  def send_newsletter
-    # NEWS_API.get_news(category, country, q, pageSize)
-
   end
 end
